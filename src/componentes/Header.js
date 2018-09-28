2544a9817369fcf54b8c2a1ea3abcca629bc26f4
CarisAvi21 <shinnytinystar@hotmail.com>
2018-09-28 13:43:24 -0500

    diseño e ingreso con mail y contraseña

---
+++ src/componentes/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+         <header>
+        <h2> My Pet
+
+        </h2> 
+        <img src = "./assets/catdog.jpg" alt = "logo" />
+
+        </header>
+    );
+}
+
+export default Header;
